7b2fb92c0833561289e7b2c03b390c61bf94fabc
Joan Macharia <joan.macharia@students.moringa>
2023-09-28 13:24:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+
+document.addEventListener('DoMContentLoaded',function(){
+    const textElement = document.getElementById('text')
+    textElement.textContent = 'This is really cool!'
+})  
